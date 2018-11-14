77a72ba10d1126a8c5801263650b81f7e4a96f7e
Daniel Casasola <casasolad86@gmail.com>
2018-11-14 15:50:07 -0600

    commig  changes to view calendar

--- client/app.js
+++ client/app.js
@@ -3,14 +3,14 @@ import userTest from './components/user/userTest'
 
 import {Navbar} from './components'
 import Routes from './routes'
-import UserTest from './components/user/userTest';
+import Dialog from './components/user/userTest';
 
 const App = () => {
   return (
     <div>
       {/* <Navbar />
       <Routes /> */}
-      <UserTest/>
+      <Dialog/>
     </div>
   )
 }
