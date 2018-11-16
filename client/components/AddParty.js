ffbd7bc2ba12a9ad8ddc9dcdca722bb7cfefb169
Daniel Casasola <casasolad86@gmail.com>
2018-11-16 10:28:13 -0600

    createdFormComp

---
+++ client/components/AddParty.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class AddParty extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default AddParty;
