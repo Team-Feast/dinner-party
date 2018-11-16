f1726f95dd923050fc1c701084c091e1fae6f474
Paul Ebreo <paulebreo@triton.edu>
2018-11-16 11:16:08 -0600

    add tests

--- server/api/parties.spec.js
+++ server/api/parties.spec.js
@@ -5,9 +5,10 @@ const db = require('../db')
 const app = require('../index')
 const Party = db.model('party')
 const User = db.model('user')
+const Guest = db.model('guest')
 const moment = require('moment')
 
-describe.only('Party API routes', () => {
+describe('Party API routes', () => {
   beforeEach(() => {
     return db.sync({force: true})
   })
@@ -32,12 +33,27 @@ describe.only('Party API routes', () => {
       const res = await agent.get('/api/parties/user/1').expect(200)
       expect(res.body.hosting.length).to.be.equal(1)
     })
-    xit('GET /api/parties/user/:userId for an attending event', async () => {
-      // const user = await createUserWithNoCart('nocart@cart.com')
-      // const res = await user.get('/api/cart').expect(200)
-      // expect(res.body.products).to.be.an('array')
-      // expect(res.body.products.length).to.be.equal(0)
-      expect(true).to.be.equal(false)
+    it('GET /api/parties/user/:userId for an attending event', async () => {
+      // host and guess are the same person
+      const host = await User.create({
+        email: 'host@test.com',
+        password: 'test'
+      })
+      // host and guess are the same person
+      const guest = await Guest.create({
+        email: 'host@test.com'
+      })
+      const party = await Party.create({
+        title: 'Test party',
+        date: moment()
+          .add(1, 'days')
+          .toDate()
+      })
+      party.setUser(host)
+      party.addGuest(guest)
+      const agent = request.agent(app)
+      const res = await agent.get('/api/parties/user/1').expect(200)
+      expect(res.body.attending.length).to.be.equal(1)
     })
   }) // end describe('/api/parties')
 }) // end describe('Party routes')
