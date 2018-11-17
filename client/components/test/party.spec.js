dec5644f3a2538fd183149994720ca6b641f1a78
Joseph Park <josephpaulpark@gmail.com>
2018-11-17 10:19:01 -0600

    fixed tests

--- client/components/test/party.spec.js
+++ client/components/test/party.spec.js
@@ -1,27 +1,27 @@
-// import React from 'react'
-// import chai, {expect} from 'chai'
-// import chaiEnzyme from 'chai-enzyme'
-// chai.use(chaiEnzyme())
-// import {shallow} from 'enzyme'
-// import SingleParty from '../SingleParty'
+import React from 'react'
+import chai, {expect} from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+chai.use(chaiEnzyme())
+import {shallow} from 'enzyme'
+import SingleParty from '../SingleParty'
 
-// xdescribe('SingleParty', () => {
-//   let singlePartyData, partyWrapper
-//   // beforeEach(‘Creatse <SingleParty /> wrapper’, () => {
-//   //  });
-//   it('includes description, location', () => {
-//     singlePartyData = {
-//       description:
-//         'Mu ciroono id zu noj douhu riwig kusgokow potji kemmadguz wefamij jacef pitumwah hecso',
-//       location: 'Uceritic ta nimzenvo jima ka.'
-//     }
-//     // creates the testable React component
-//     partyWrapper = shallow(<SingleParty data={singlePartyData} />)
-//     expect(partyWrapper.find('p')).to.have.html(
-//       '<p>Mu ciroono id zu noj douhu riwig kusgokow potji kemmadguz wefamij jacef pitumwah hecso</p>'
-//     )
-//     expect(partyWrapper.find('h6')).to.have.html(
-//       '<h6>Uceritic ta nimzenvo jima ka.</h6>'
-//     )
-//   })
-// })
+xdescribe('SingleParty', () => {
+  let singlePartyData, partyWrapper
+  // beforeEach(‘Creatse <SingleParty /> wrapper’, () => {
+  //  });
+  it('includes description, location', () => {
+    singlePartyData = {
+      description:
+        'Mu ciroono id zu noj douhu riwig kusgokow potji kemmadguz wefamij jacef pitumwah hecso',
+      location: 'Uceritic ta nimzenvo jima ka.'
+    }
+    // creates the testable React component
+    partyWrapper = shallow(<SingleParty data={singlePartyData} />)
+    expect(partyWrapper.find('p')).to.have.html(
+      '<p>Mu ciroono id zu noj douhu riwig kusgokow potji kemmadguz wefamij jacef pitumwah hecso</p>'
+    )
+    expect(partyWrapper.find('h6')).to.have.html(
+      '<h6>Uceritic ta nimzenvo jima ka.</h6>'
+    )
+  })
+})
