edcf4a3b13e358720cf155e5ea90cd165e63c744
Andrew Ferguson <andrewmf@umich.edu>
2018-11-19 08:27:27 -0600

    nodemailer done and guest POST route

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,7 @@ module.exports = router
 
 router.use('/users', require('./users'))
 router.use('/parties', require('./parties'))
+router.use('/guests', require('./guests'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
