fac57d1e4b9eeed0b2720e6a72111d05faa3c02b
Joseph Park <josephpaulpark@gmail.com>
2018-11-27 15:51:59 -0600

    guest is able to update status n single party view

--- client/components/SingleParty.js
+++ client/components/SingleParty.js
@@ -2,6 +2,7 @@ import React, {Component, Fragment} from 'react'
 import {connect} from 'react-redux'
 import {
   getParty,
+  putGuest,
   putGuestStatus,
   getGuestStatus,
   getGuests,
@@ -11,7 +12,6 @@ import {
 import {GuestList, ItemList, Gallery, Map} from '.'
 import moment from 'moment'
 import history from '../history'
-import axios from 'axios'
 
 // MATERIAL UI IMPORTS
 import PropTypes from 'prop-types'
@@ -20,32 +20,22 @@ import CardHeader from '@material-ui/core/CardHeader'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
 import IconButton from '@material-ui/core/IconButton'
-import MoreVertIcon from '@material-ui/icons/MoreVert'
 import {withStyles} from '@material-ui/core/styles'
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import Checkbox from '@material-ui/core/Checkbox'
-import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank'
-import CheckBoxIcon from '@material-ui/icons/CheckBox'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
-import Switch from '@material-ui/core/Switch'
 import green from '@material-ui/core/colors/green'
 import red from '@material-ui/core/colors/red'
-import Radio from '@material-ui/core/Radio'
-import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked'
-import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked'
 import Create from '@material-ui/icons/Create'
 import CalendarToday from '@material-ui/icons/CalendarToday'
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
-import Avatar from '@material-ui/core/Avatar'
 import deepOrange from '@material-ui/core/colors/deepOrange'
 
 const toonavatar = require('cartoon-avatar')
@@ -88,29 +78,35 @@ class SingleParty extends Component {
     selectedValue: 'invited'
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     const {guestPartyToken, partyId} = this.props.match.params
 
     this.props.getParty(partyId)
-    this.props.getGuests(partyId)
+    await this.props.getGuests(partyId)
     this.props.getItems(partyId)
 
-    if (guestPartyToken) this.props.getGuestStatus(guestPartyToken, partyId)
-  }
+    if (guestPartyToken) {
+      const guest = this.props.guests.find(
+        ele => ele.guestPartyToken === guestPartyToken
+      )
 
-  componentDidUpdate(prevProps) {
-    if (this.props.guestStatus !== prevProps.guestStatus) {
-      this.setState({selectedValue: this.props.guestStatus})
+      this.setState({selectedValue: guest.status})
     }
   }
+
   handleChange = event => {
-    const rsvp = event.target.name
-    this.setState({selectedValue: rsvp})
+    const status = event.target.name
+    this.setState({selectedValue: status})
     const {guestPartyToken} = this.props.match.params
-    this.props.putGuestStatus(guestPartyToken, rsvp)
+
+    const guest = this.props.guests.find(
+      ele => ele.guestPartyToken === guestPartyToken
+    )
+
+    this.props.putGuest(guest.id, {status})
   }
 
-  handleAddToCalendar = async () => {
+  handleAddToCalendar = () => {
     const {loggedInUser, party, guests} = this.props
     const guest = guests.find(ele => ele.email === loggedInUser.email)
     this.props.postToCalendar(guest.id, party)
@@ -122,7 +118,6 @@ class SingleParty extends Component {
       description,
       location,
       imageUrl,
-      status,
       user,
       date,
       id,
@@ -216,11 +211,6 @@ class SingleParty extends Component {
 
           <ExpansionPanel>
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-              {/* <Avatar className={classes.avatar} src={"/images/default-user.jpg"}/>
-              <Avatar className={classes.avatar}src={url}/>
-              <Avatar className={classes.avatar}src={url}/>
-              <Avatar className={classes.avatar}src={url}/> */}
-
               <Typography className={classes.heading}>
                 {`Guest List (Attending: ${
                   guests.filter(guest => guest.status === 'attending').length
@@ -259,7 +249,6 @@ const mapState = state => ({
   party: state.party,
   guests: state.guests,
   items: state.items,
-  guestStatus: state.guestStatus,
   loggedInUser: state.user
 })
 
@@ -267,6 +256,7 @@ const mapDispatch = dispatch => ({
   postToCalendar: (guestId, party) => dispatch(postToCalendar(guestId, party)),
   getParty: partyId => dispatch(getParty(partyId)),
   getGuests: partyId => dispatch(getGuests(partyId)),
+  putGuest: (guestId, status) => dispatch(putGuest(guestId, status)),
   getItems: partyId => dispatch(getItems(partyId)),
   putGuestStatus: (guestPartyToken, status) =>
     dispatch(putGuestStatus(guestPartyToken, status)),
